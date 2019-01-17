6a3771d8c8dcce425f3d2f9371188ffdb5ebc540
Profiteam <31069986+Profiteam@users.noreply.github.com>
2019-01-17 15:59:31 +0300

    add auth requests

--- src/app/common/services/auth.service.ts
+++ src/app/common/services/auth.service.ts
@@ -5,49 +5,30 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { LocalStorageHelper } from '../helpers';
 import * as moment from 'moment';
 import { Router } from '@angular/router';
-import { SignInRequest } from '../models/request';
+import { SignInRequest, SignUpRequest } from '../models/request';
 import { SignInResponse } from '../models/response/sign-in-response.model';
+import { ResponseModel } from '../models/response';
 
 @Injectable()
 export class AuthService extends BaseHttpService {
+
     private loginSubject: BehaviorSubject<boolean>;
 
-    constructor(
-        private http: Http,
-        private router: Router
-    ) {
-        super();
-
-        this.checkAuth();
-    }
-
-    public authorization(request: SignInRequest): Observable<SignInResponse> {
-        return this.http.post(`${this.apiUrl}/account/sign-in`, request)
-            .map(response => this.saveToken(response.json()));
-    }
-
-    private saveToken(response: SignInResponse): SignInResponse {
-        if (!response) return;
-        
-        if (response.securityToken) {
-            LocalStorageHelper.saveToken(response.securityToken);
-            this.loginSubject.next(true);
-        }
-
-        return response;
-    }
-
-    public logout() {
-        LocalStorageHelper.removeToken();
-        this.loginSubject.next(false);
-        this.router.navigate(['/authorization']);
-    }
-
+    // Check is user logged in
     private checkAuth() {
         const token = LocalStorageHelper.getToken();
         this.loginSubject = new BehaviorSubject<boolean>(token ? true : false);
     }
 
+    // Logout method
+    public logout() {
+        LocalStorageHelper.removeToken();
+
+        this.loginSubject.next(false);
+        this.router.navigate(['/authorization']);
+    }
+
+    // Check is user logged in now
     public get isLoggedIn(): Observable<boolean> {
         return this.loginSubject.asObservable()
             .map(logged => {
@@ -64,6 +45,7 @@ export class AuthService extends BaseHttpService {
             });
     }
 
+    // Check is token expired
     public get expired(): boolean {
         const token = LocalStorageHelper.getToken();
 
@@ -73,5 +55,35 @@ export class AuthService extends BaseHttpService {
 
         const result = ((!token) || (!token.expireDate) || isTokenExpired);
         return result;
+    }
+
+    // Save token to localStorage
+    private saveToken(response: SignInResponse): SignInResponse {
+        if (!response) return;
+        
+        if (response.securityToken) {
+            LocalStorageHelper.saveToken(response.securityToken);
+            this.loginSubject.next(true);
+        }
+
+        return response;
+    }
+
+    constructor(
+        private router: Router
+    ) {
+        super();
+
+        this.checkAuth();
+    }
+
+    public registration(request: SignUpRequest): Observable<ResponseModel> {
+        return this.post(`${this.apiUrl}/users/sign-up`, request)
+            .map(response => response.json());
+    }
+
+    public authorization(request: SignInRequest): Observable<SignInResponse> {
+        return this.post(`${this.apiUrl}/users/sign-in`, request)
+            .map(response => this.saveToken(response.json()));
     }
 }
--- src/app/common/services/base-http.service.ts
+++ src/app/common/services/base-http.service.ts
@@ -1,5 +1,33 @@
 import { environment } from 'src/environments/environment';
+import { Http, RequestOptionsArgs, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import { RequestHelper, LocalStorageHelper } from '../helpers';
 
 export class BaseHttpService {
+    private http: Http;
     public apiUrl: string = environment.apiEndpoint;
+
+    get(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        if (!options)
+            options = RequestHelper.constructRequestOptions();
+
+        const securityToken = LocalStorageHelper.getToken();
+        if (securityToken) {
+            options.headers.set('Authorization', securityToken.token)
+        }
+
+        return this.http.get(url, options);
+    }
+
+    post(url: string, body: any, options?: RequestOptionsArgs): Observable<Response> {
+        if (!options)
+            options = RequestHelper.constructRequestOptions();
+
+        const securityToken = LocalStorageHelper.getToken();
+        if (securityToken) {
+            options.headers.set('Authorization', securityToken.token)
+        }
+
+        return this.http.post(url, body, options);
+    }
 }
